9b1148125f8bdc7177d8bde4716b1a7affa3e236
Eric Ma <ericma1294@gmail.com>
2020-10-13 17:40:04 +0900

    test

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Is there lunch today!?!?!!?
+        </p>
+        <h2>NO!</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
